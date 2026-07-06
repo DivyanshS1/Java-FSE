84ba09be1868d1fb9fc7380e98fa427cfc7d844d
DivyanshS1 <singhaldivyansh220@gmail.com>
2026-07-06 17:51:24 +0530

    Exercise-9

---
+++ Week-3/LibraryManagementInitializer/src/main/java/com/library/LibraryManagementInitializer/LibraryManagementInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.library.LibraryManagementInitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementInitializerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementInitializerApplication.class, args);
+    }
+
+}
