e2de1d004a8bba730e196ab1997082406d9bf4ad
DivyanshS1 <singhaldivyansh220@gmail.com>
2026-07-06 13:51:21 +0530

    Exercise-5

---
+++ Week-3/LibraryManagement1/src/main/java/com/div/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.div.repository;
+
+public class BookRepository {
+    public void getBooks() {
+        System.out.println("Fetching books from the repository...");
+    }
+}
---
+++ Week-3/LibraryManagement1/src/main/java/com/div/service/BookService.java
@@ -0,0 +1,18 @@
+package com.div.service;
+
+import com.div.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    // Setter Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayBooks() {
+        System.out.println("Book Service: Processing request...");
+        bookRepository.getBooks();
+    }
+
+}
