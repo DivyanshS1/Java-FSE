03b1901d2147011950df304964bc43b27b4f65b2
DivyanshS1 <singhaldivyansh220@gmail.com>
2026-07-07 19:01:43 +0530

    Exercise-3

---
+++ Week-2/EmployeeManagementSystem/src/main/java/com/div/EmployeeManagementSystem/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.div.EmployeeManagementSystem.repository;
+
+import com.div.EmployeeManagementSystem.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findByName(String name);
+
+    Employee findByEmail(String email);
+}
