b115a9d51d37e5c338a0664aedf86637500b505a
DivyanshS1 <singhaldivyansh220@gmail.com>
2026-07-07 22:49:45 +0530

    Exercise-6

--- Week-2/EmployeeManagementSystem/src/main/java/com/div/EmployeeManagementSystem/controller/EmployeeController.java
+++ Week-2/EmployeeManagementSystem/src/main/java/com/div/EmployeeManagementSystem/controller/EmployeeController.java
@@ -2,6 +2,10 @@ package com.div.EmployeeManagementSystem.controller;
 
 import com.div.EmployeeManagementSystem.entity.Employee;
 import com.div.EmployeeManagementSystem.repository.EmployeeRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,16 +19,31 @@ public class EmployeeController {
     public EmployeeController(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
+    @GetMapping
+    public Page<Employee> getEmployees(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction) {
+
+        Sort sort = direction.equalsIgnoreCase("desc")
+                ? Sort.by(sortBy).descending()
+                : Sort.by(sortBy).ascending();
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return employeeRepository.findAll(pageable);
+    }
 
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
 
-    @GetMapping
-    public List<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
-    }
+//    @GetMapping
+//    public List<Employee> getAllEmployees() {
+//        return employeeRepository.findAll();
+//    }
 
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable Long id) {
