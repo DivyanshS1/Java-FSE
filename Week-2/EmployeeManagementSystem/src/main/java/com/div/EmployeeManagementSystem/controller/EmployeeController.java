f902073cc4b5352b19156e73b56e3c15ce8d3c10
DivyanshS1 <singhaldivyansh220@gmail.com>
2026-07-07 19:16:34 +0530

    Exercise-4

---
+++ Week-2/EmployeeManagementSystem/src/main/java/com/div/EmployeeManagementSystem/controller/EmployeeController.java
@@ -0,0 +1,44 @@
+package com.div.EmployeeManagementSystem.controller;
+
+import com.div.EmployeeManagementSystem.entity.Employee;
+import com.div.EmployeeManagementSystem.repository.EmployeeRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        employee.setId(id);
+        return employeeRepository.save(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
