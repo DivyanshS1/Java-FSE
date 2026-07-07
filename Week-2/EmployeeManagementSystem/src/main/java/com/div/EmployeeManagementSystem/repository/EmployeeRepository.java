e735d2da08101019b9317bb7f69c0a4c914b17ed
DivyanshS1 <singhaldivyansh220@gmail.com>
2026-07-07 23:19:24 +0530

    Exercise-8

--- Week-2/EmployeeManagementSystem/src/main/java/com/div/EmployeeManagementSystem/repository/EmployeeRepository.java
+++ Week-2/EmployeeManagementSystem/src/main/java/com/div/EmployeeManagementSystem/repository/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package com.div.EmployeeManagementSystem.repository;
 
 import com.div.EmployeeManagementSystem.entity.Employee;
+import com.div.EmployeeManagementSystem.projection.EmployeeDTO;
+import com.div.EmployeeManagementSystem.projection.EmployeeView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,14 +10,10 @@ import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-    List<Employee> findByDepartmentId(Long departmentId);
+    // Interface-based Projection
+    List<EmployeeView> findBy();
 
-    // Custom Query using @Query
-    @Query("SELECT e FROM Employee e WHERE e.email LIKE %?1%")
-    List<Employee> searchByEmail(String keyword);
-
-    // Named Queries
-    List<Employee> findByName(String name);
-
-    Employee findByEmail(String email);
+    // Class-based Projection using constructor expression
+    @Query("SELECT new com.div.EmployeeManagementSystem.projection.EmployeeDTO(e.name, e.email) FROM Employee e")
+    List<EmployeeDTO> getEmployeeDTOs();
 }
