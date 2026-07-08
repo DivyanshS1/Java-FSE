3a9500fba8c7f71766cfa071be5ad5c722385d02
DivyanshS1 <singhaldivyansh220@gmail.com>
2026-07-08 13:31:01 +0530

    Task-1

--- Week-1/spring-learn/src/test/java/com/cognizant/spring_learn/SpringLearnApplicationTests.java
+++ Week-1/spring-learn/src/test/java/com/cognizant/spring_learn/SpringLearnApplicationTests.java
@@ -1,23 +1,13 @@
 package com.cognizant.spring_learn;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class SpringLearnApplicationTests {
+@SpringBootTest
+class SpringLearnApplicationTests {
 
-	private static final Logger LOGGER =
-			LoggerFactory.getLogger(SpringLearnApplicationTests.class);
-
-	public static void main(String[] args) {
-
-		LOGGER.info("Application Started");
-
-		SpringApplication.run(SpringLearnApplicationTests.class, args);
-
-		LOGGER.info("Application Running");
+	@Test
+	void contextLoads() {
 	}
 
 }
