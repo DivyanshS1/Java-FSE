2c4d4dbc8b1e85751082faaeefaa62b16646e5e9
DivyanshS1 <singhaldivyansh220@gmail.com>
2026-07-14 22:59:25 +0530

    Assignment-3

---
+++ Week-4/scorecalculatorapp/src/App.js
@@ -0,0 +1,14 @@
+import { CalculateScore } from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore Name={"Divyansh"}
+      School={"KIIT University"}
+      Total={284}
+      goal={3}/>
+    </div>
+  );
+}
+
+export default App;
