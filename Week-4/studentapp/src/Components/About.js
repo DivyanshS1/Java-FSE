e43db5ab062b3d69db6756f1ffe0ac147d9248da
DivyanshS1 <singhaldivyansh220@gmail.com>
2026-07-14 22:19:00 +0530

    Assignment-2

---
+++ Week-4/studentapp/src/Components/About.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class About extends Component {
+    render() {
+        <div>
+            <h3>Welcome to the About Page of Student Management Portal</h3>
+        </div>
+    }
+}
+export default About;
